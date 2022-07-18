1e035b5448fc0ab2768762b1774bbbeee90d186f
Festison <105289311+Festison@users.noreply.github.com>
2022-07-18 16:33:55 +0900

    Add files via upload

---
+++ 7. 실습/TextRPG/1. TextRPG (OOP).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include"Game.h"
+using namespace std;
+
+int main()
+{
+	srand((unsigned int)time(nullptr));
+	Game game;
+	game.Init();
+
+	while (true)
+	{
+		game.Update();
+	}
+
+	return 0;
+
+}
