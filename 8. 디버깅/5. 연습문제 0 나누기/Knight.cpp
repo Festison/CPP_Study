f745ba71ea82457bc55b93349bb51eee9a7c9a30
Festison <105289311+Festison@users.noreply.github.com>
2022-07-20 13:02:54 +0900

    Add files via upload

---
+++ 8. 디버깅/5. 연습문제 0 나누기/Knight.cpp
@@ -0,0 +1,58 @@
+#include "Knight.h"
+#include <iostream>
+using namespace std;
+
+// [사양서] 기본값 Hp=100, Attack=10
+Knight::Knight() : _hp(100), _maxHp(100), _attack(10)
+{
+
+}
+
+Knight::Knight(int hp) : _hp(hp), _maxHp(hp), _attack(10)
+{
+
+}
+
+Knight::~Knight()
+{
+
+}
+
+void Knight::AddHp(int value)
+{
+	_hp += value;
+	if (_hp < 0)
+		_hp = 0;
+	if (_hp > _maxHp)
+		_hp = _maxHp;
+}
+
+bool Knight::IsDead()
+{
+	return (_hp <= 0);
+}
+
+int Knight::GetAttackDamage()
+{
+	// hp 50% 이하 => maxHp / hp가 2 이상
+	int damage = _attack;
+
+	if (_maxHp == 0)
+	{
+		return damage;
+	}
+	int percentage = (_hp * 100) / _maxHp;
+
+	if (percentage<=50)
+	{
+		damage *= 2;
+	}
+
+	return damage;
+}
+
+void Knight::PrintInfo()
+{
+	cout << "HP: " << _hp << endl;
+	cout << "ATT: " << _attack << endl;
+}
